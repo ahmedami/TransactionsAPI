89c19daeb621093be03893e49a443cd61c23f44d
Ahmed Abdelrahman <eng.ahmedami@gmail.com>
2016-02-22 03:14:35 +0100

    DEV-2 minor change

--- src/main/java/org/ahmed/transactions/entity/Type.java
+++ src/main/java/org/ahmed/transactions/entity/Type.java
@@ -8,7 +8,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 public class Type {
 	
-	private transient String name;
+	private String name;
 	private Set<Long> transactionsIds= new HashSet<>();
 	
 	public Type(String name, Long transactionId) {
@@ -28,6 +28,29 @@ public class Type {
 	}
 	public Set<Long> getTransactionsIds() {
 		return transactionsIds;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Type other = (Type) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}	
 	
 }
